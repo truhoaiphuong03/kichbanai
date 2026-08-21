478c622630e33652478acf4e098bf576c3d175c0
truhoaiphuong03 <truhoaiphuong03@gmail.com>
2026-08-21 08:06:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if prompt := st.chat_input("Nhập yêu cầu, kịch bản, câu hỏi..."):
 
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel("gemini-1.5-flash")
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
             response = model.generate_content(prompt)
             reply = response.text
 
